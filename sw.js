908dd9248147b8032c321df32a1ea17bbd3ef79b
DrinkTracker <154763093+DrinkTracker@users.noreply.github.com>
2023-12-24 20:04:07 -0600

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,10 +1,1 @@
-self.addEventListener('push', function(event) {
-  const pushData = event.data.json();
-  const title = pushData.title;
-  const options = {
-    body: pushData.body,
-    icon: 'path/to/your/icon.png',
-    badge: 'path/to/your/badge-icon.png'
-  };
-  event.waitUntil(self.registration.showNotification(title, options));
-});
+importScripts("https://cdn.pushalert.co/sw-65514.js");
